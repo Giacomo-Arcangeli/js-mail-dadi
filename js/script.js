f6f48711bdfffa6024d3776184648ce8dddf6673
Giacomo-Arcangeli <giacomoarcangeli1997@gmail.com>
2023-05-31 18:50:41 +0200

    mail (todo fix)

--- js/script.js
+++ js/script.js
@@ -3,16 +3,19 @@ const mailList = ['giacomo@live.it', 'matteo@live.it', 'luca@live.it'];
 console.table (mailList);
 
 // chiedo all'utente di inserire una mail
- const userMail = prompt('Inserisci una mail');
+ const insertMail = prompt('Inserisci una mail');
 
 // controllo che la mail dell'utente sia nella lista
-
 for ( let i=0; i < mailList.length; i++){
-   const mail = mailList[''];
-    // SE la mail dell'utente è presente in lista
-    if(userMail === mail){
+
+   userMail = mailList[i];
+
+// SE la mail dell'utente è presente in lista
+    if(userMail === insertMail){
+// scrivo benvenuto
         console.log('Benvenuto');
     } else{
+// scrivo mail non trovata
         console.log('Mail non trovata');
     }
 }
